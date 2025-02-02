3775a4f8bc325dc9d55bfbbd6ca86bcc5d1e1528
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2025-02-02 10:09:21 +0530

    heart anim working - note, use tailwind animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%, 75%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+        },
+        heartAnimation: {
+          '0%': {
+            opacity: 1,
+            transform: 'translate(-50%, -50%) scale(1)',
+          },
+          '100%': {
+            opacity: 0,
+            transform: 'translate(-50%, -150%) scale(2)',
+          },
+        },
+      },
+      
+      animation: {
+        shake: 'shake 0.5s',
+        heartAnimation: 'heartAnimation 1s ease-out forwards',
+      },
     },
   },
   plugins: [],
