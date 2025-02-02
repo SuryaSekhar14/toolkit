c1c3d4ede980af5cb70a86ed9a043010d3753cc4
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2025-02-02 09:53:02 +0530

    beasic heart on click

--- app/components/AppCard.tsx
+++ app/components/AppCard.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { FaHeart, FaShare } from "react-icons/fa";
 import Link from "next/link";
+import Image from "next/image";
 
 interface AppCardProps {
 	icon?: React.ReactNode | string;
@@ -25,9 +26,22 @@ const AppCard: React.FC<AppCardProps> = ({
 
 	const renderIcon = () => {
 		if (typeof icon === 'string') {
-			return <img src={icon} alt="icon" className="mr-2 w-6 h-6" />;
+			return <Image src={icon} alt="icon" className="mr-2 w-6 h-6" width={24} height={24} />;
 		}
 		return icon;
+	};
+
+	const emitHearts = (e: React.MouseEvent) => {
+		const heart = document.createElement('div');
+		heart.className = 'heart';
+		heart.style.position = 'fixed';
+		heart.style.left = `${e.clientX}px`;
+		heart.style.top = `${e.clientY}px`;
+		heart.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" width="20px" height="20px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>';
+		document.body.appendChild(heart);
+		setTimeout(() => {
+			heart.remove();
+		}, 1000);
 	};
 
 	return (
@@ -44,14 +58,12 @@ const AppCard: React.FC<AppCardProps> = ({
 				</div>
 			</Link>
 			<div className="flex justify-end gap-2 p-2">
-				<span className="text-gray-600 dark:text-gray-400">
+				<button className="text-gray-600 dark:text-gray-400" onClick={emitHearts}>
 					<FaHeart />
-				</span>
-				<span className="text-gray-600 dark:text-gray-400">
-					<button onClick={() => navigator.clipboard.writeText(window.location.origin + link)}>
-						<FaShare />
-					</button>
-				</span>
+				</button>
+				<button className="text-gray-600 dark:text-gray-400" onClick={() => navigator.clipboard.writeText(window.location.origin + link)}>
+					<FaShare />
+				</button>
 			</div>
 		</div>
 	);
